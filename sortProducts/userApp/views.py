f99ce2c812e15e5eb241278c999ccfc3c8094b52
SureshMaloth <suresh.marolix99@gamil.com>
2024-01-30 11:19:40 +0530

    sort products

--- sortProducts/userApp/views.py
+++ sortProducts/userApp/views.py
@@ -1,17 +1,21 @@
 
 
-# Create your views here.
+
 from rest_framework import generics
 from .models import Product, Feedback
+from rest_framework import status
+from rest_framework.response import Response
 from .serializers import ProductSerializer, FeedbackSerializer
 from rest_framework.filters import OrderingFilter,SearchFilter
-
+from django_filters.rest_framework import DjangoFilterBackend
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends=(SearchFilter,OrderingFilter)
+    filter_backends=(DjangoFilterBackend,SearchFilter,OrderingFilter)
+    filterset_fields=['category']
     search_fields=('product_name','price','category')
     ordering_fields=('product_name','price','id')
+    
 
 
 class FeedbackList(generics.ListCreateAPIView):
